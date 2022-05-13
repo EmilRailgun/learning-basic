522f25e4970fa11bb2d13402e4f30a29c815b6ad
EmilRailgun <hoangominh01@gmail.com>
2022-05-13 10:03:01 +0700

    master

---
+++ app/Models/Listing.php
@@ -0,0 +1,23 @@
+﻿<?php
+namespace App\Models;
+
+class Listing
+    {
+        public static function all()
+        {
+            return [
+                [
+                    'id' => 1,
+                    'title' => 'Listing 1',
+                    'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod, nunc euismod consectetur convallis, nisi nunc ultricies nisi, euismod consectetur nisi nisi euismod.',
+                ],
+                [
+                    'id' => 2,
+                    'title' => 'Listing 2',
+                    'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod, nunc euismod consectetur convallis, nisi nunc ultricies nisi, euismod consectetur nisi nisi euismod.',
+                ],
+            ];
+        }
+    }
+
+
